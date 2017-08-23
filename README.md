25472863bf6e2126f6d7f58a876c486305a07f1f
Jan Horecny <support@zonemedia.sk>
2017-08-23 14:43:19 +0200

    Options.class.php Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # acf-helpers
 Simple ACF helpers we are using daily
+
+## options.class.php
+Copy and include this class in your current WordPress theme. This class supports WPML for loading different language options. 
+
+**Usage:**
+
+*1. Get option value from current language*
+```
+Options::get('your_options_key');
+```
+
+*2. Get option value from different (not current) language*
+```
+Options::get('your_options_key', 'language_code');
+```
+
+*3. Get option value from default (primary) language*
+```
+Options::get_default('your_options_key');
+```
---
+++ options.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * ACF Options class
+ *
+ * usage: Options::get('name_of_option');
+ *
+ * @version 1.0.0
+ * @link https://www.advancedcustomfields.com/resources/options-page/
+ * @author Jan Horecny <horecny@zonemedia.sk>
+ */
+Class Options {
+
+	/**
+	 * Returns Option value from given language
+	 *
+	 * @param bool|string $name option key name
+	 * @param bool|string $language language, default is current WPML language
+	 *
+	 * @return mixed
+	 */
+	public static function get( $name = false, $language = false ) {
+		if ( defined( 'ICL_LANGUAGE_CODE' ) && $language && $language !== ICL_LANGUAGE_CODE ) {
+			return get_field( $name, 'options_' . $language );
+		} else {
+			return get_field( $name, 'option' );
+		}
+	}
+
+	/**
+	 * Returns Option value from default WPML language
+	 *
+	 * @param $name option key name
+	 *
+	 * @return mixed
+	 */
+	public static function get_default( $name ) {
+		add_filter( 'acf/settings/current_language', 'cl_acf_set_language', 100 );
+		$option = get_field( $name, 'option' );
+		remove_filter( 'acf/settings/current_language', 'cl_acf_set_language', 100 );
+
+		return $option;
+	}
+
+}
